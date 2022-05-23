e56399f9b530d018224856c971f62b1056d5c8eb
znb <znb20010119@163.com>
2022-05-24 07:51:25 +0800

    2022年5月24日 07:51:22

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 export default {
-    //1 添加课程信息功能
+    //1 添加课程信息
     addCourseInfo(courseInfo) {
         return request({
             url: '/eduservice/course/addCourseInfo',
@@ -9,10 +9,26 @@ export default {
           })
     },
     //2 查询所有讲师
-    getListTeacher(){
+    getListTeacher() {
         return request({
             url: '/eduservice/teacher/findAll',
             method: 'get'
           })
+    },
+    //根据课程id查询课程基本信息
+    getCourseInfoId(id) {
+        return request({
+            url: '/eduservice/course/getCourseInfo/'+id,
+            method: 'get'
+          })
+    },
+    //修改课程信息
+    updateCourseInfo(courseInfo) {
+        return request({
+            url: '/eduservice/course/updateCourseInfo',
+            method: 'post',
+            data: courseInfo
+          })
     }
+
 }
